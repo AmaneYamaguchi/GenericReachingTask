0db574ec92bf0fa31cf986d2de3bf966fc62f423
AmaneYamaguchi <yamabach@icloud.com>
2025-01-26 18:00:12 +0900

    add mortality to normal reaching target

--- Assets/GenericReachingTask/Runtime/PooledReachingTarget.cs
+++ Assets/GenericReachingTask/Runtime/PooledReachingTarget.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,12 +15,37 @@ namespace GenericReachingTask
             set => m_pool = value;
         }
 
+        /// <summary>
+        /// オブジェクトが非アクティブになったときに呼ばれるイベント
+        /// </summary>
         public UnityEvent OnDeactivate => m_onDeactivate;
         private UnityEvent m_onDeactivate = new();
+
+        /// <summary>
+        /// オブジェクトが生成されてから自動で消えるまでの時間
+        /// </summary>
+        [SerializeField]
+        private float m_lifeTime = float.MaxValue;
+        /// <summary>
+        /// オブジェクトが生成されてから自動で消えるまでの時間。
+        /// デフォルトでは無限（float.MaxValue）。Initialize()の直後に設定すること。
+        /// </summary>
+        public float LifeTime
+        {
+            get => m_lifeTime;
+            set => m_lifeTime = value;
+        }
+        private float m_currentLifeTime = 0f;
+        /// <summary>
+        /// オブジェクトが寿命を過ぎているかどうか
+        /// </summary>
+        public bool IsOutOfLife => m_lifeTime <= m_currentLifeTime;
 
         public void Initialize()
         {
             m_onDeactivate.RemoveAllListeners();
+            m_currentLifeTime = 0f;
+            m_lifeTime = float.MaxValue;
         }
         public void Deactivate()
         {
@@ -28,6 +53,18 @@ namespace GenericReachingTask
             m_pool.Release(this);
         }
 
+        /// <summary>
+        /// Updateはゲームオブジェクトが有効な場合にのみ呼ばれるため、
+        /// Deactivate()後から次のInitialize()までは呼ばれない。
+        /// </summary>
+        void Update()
+        {
+            m_currentLifeTime += Time.deltaTime;
+            if (IsOutOfLife)
+            {
+                Deactivate();
+            }
+        }
         void OnTriggerEnter(Collider other)
         {
             Deactivate();
